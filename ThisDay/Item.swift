8eba9fd946ea48439cb3797eea752a69f9f4f431
Roman Yefimets <romanyefimets@gmail.com>
2024-03-26 14:25:02 -0700

    Initial Commit

---
+++ ThisDay/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ThisDay
+//
+//  Created by Roman Yefimets on 3/26/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
